9975dce77d83d2a01c67b2998b45ed3900c35602
Prem-K-Tamang <ktamangprem@gmail.com>
2021-01-15 00:48:11 +0545

    Static files updated

--- imdb/settings.py
+++ imdb/settings.py
@@ -110,8 +110,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'imdb/static'),
 ]
 
 
